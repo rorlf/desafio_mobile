068b351e89bde58182e36b12f19514b6a02fde69
Rodrigo <rodrigo@gtacsolutions.com>
2021-08-29 00:05:15 -0300

    feat(navigators): add new screens in AppNavigator

--- src/navigators/AppNavigator/index.tsx
+++ src/navigators/AppNavigator/index.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import routes from 'navigators/routes';
-import LoginScreen from 'screens/LoginScreen';
+import {
+  CardStyleInterpolators,
+  createStackNavigator
+} from '@react-navigation/stack';
+import { RootStackParamList } from './types';
 
-const Stack = createStackNavigator();
+// Screens
+import LoginScreen from 'screens/LoginScreen';
+import RegisterScreen from 'screens/RegisterScreen';
+import LoadingScreen from 'screens/LoadingScreen';
+
+// Context
+import { useAuth } from 'contexts/Auth';
+import HomeScreen from 'screens/HomeScreen';
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
+  const { isInitializing, isAuthenticated } = useAuth();
+
+  if (isInitializing) {
+    return <LoadingScreen />;
+  }
+
   return (
     <Stack.Navigator
       screenOptions={{
         gestureEnabled: false,
-        headerShown: false
+        headerShown: false,
+        cardStyleInterpolator: CardStyleInterpolators.forFadeFromCenter
       }}>
-      <Stack.Screen name={routes.LoginScreen} component={LoginScreen} />
+      {isAuthenticated ? (
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      ) : (
+        <>
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
--- src/navigators/routes.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  LoginScreen: 'LoginScreen'
-};
