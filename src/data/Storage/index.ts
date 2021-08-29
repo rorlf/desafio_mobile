96cf4d31f386a12bda9698ba122cdaeeea36b46b
Rodrigo <rodrigo@gtacsolutions.com>
2021-08-29 00:01:55 -0300

    feat(data): add Storage

---
+++ src/data/Storage/index.ts
@@ -0,0 +1,41 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+function convertItemToData(item) {
+  const data = JSON.parse(item);
+  return data;
+}
+
+function convertDataToItem(data) {
+  const item = JSON.stringify(data);
+  return item;
+}
+
+async function storeData(key, data) {
+  try {
+    const item = convertDataToItem(data);
+    await AsyncStorage.setItem(key, item);
+  } catch (e) {}
+}
+
+async function getData(key) {
+  try {
+    const item = await AsyncStorage.getItem(key);
+    if (item !== null) {
+      const data = convertItemToData(item);
+      return data;
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
+
+async function removeData(key) {
+  await AsyncStorage.removeItem(key);
+}
+
+const constants = Object.freeze({
+  LAST_ACCESS_POSITION: '@last_access_position'
+});
+
+export { storeData, getData, removeData, constants };
